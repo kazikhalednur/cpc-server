ccd6b2adeb20acced033200fd4d6611c2fe5addb
Kazi Khaled Nur <kazikhalednur@gmail.com>
2025-10-16 10:40:46 +0600

    Update filter of contests

--- contests/filters.py
+++ contests/filters.py
@@ -1,4 +1,5 @@
 import django_filters
+from django.utils import timezone
 
 from .models import Contest
 
@@ -20,9 +21,9 @@ class ContestFilter(django_filters.FilterSet):
     search = django_filters.CharFilter(
         field_name="title", lookup_expr="icontains", help_text="Search by contest title"
     )
-    status = django_filters.ChoiceFilter(
-        choices=Contest.Status.choices,
+    status = django_filters.CharFilter(
         help_text="Filter by contest status",
+        method="filter_by_status",
     )
     start_time_from = django_filters.DateTimeFilter(
         field_name="start_time",
@@ -57,3 +58,13 @@ class ContestFilter(django_filters.FilterSet):
             "end_time_from",
             "end_time_to",
         ]
+
+    def filter_by_status(self, queryset, name, value):
+        if value == "UPCOMING":
+            return queryset.filter(start_time__gte=timezone.now())
+        elif value == "ONGOING":
+            return queryset.filter(
+                start_time__lte=timezone.now(), end_time__gte=timezone.now()
+            )
+        elif value == "COMPLETED":
+            return queryset.filter(end_time__lte=timezone.now())
