a6e4f8ef9dc33883dfc141b4c5d3c7c24d184998
Kazi Khaled Nur <kazikhalednur@gmail.com>
2025-10-17 10:48:23 +0600

    Update database settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -107,6 +107,8 @@ DATABASES = {
         "NAME": config("DB_NAME", cast=str),
         "USER": config("DB_USER", cast=str),
         "PASSWORD": config("DB_PASSWORD", cast=str),
+        "HOST": config("DB_HOST", cast=str),
+        "PORT": config("DB_PORT", cast=int),
         "OPTIONS": {
             "autocommit": True,
         },
